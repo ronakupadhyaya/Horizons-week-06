7515247778731d183d8003c98f7277ff860a8a2c
joostkamermans <JoostKamermans1@gmail.com>
2016-07-05 14:07:51 -0400

    asdf
    asdf

--- day1/static-login/client/index.js
+++ day1/static-login/client/index.js
@@ -7,6 +7,43 @@ var ReactDOM = require('react-dom');
 //   Password: <input type="password" name="password">
 //   <input type="submit">
 // </form>
-var element = React.createElement('form', /* YOUR CODE HERE */);
 
-ReactDOM.render(element, document.getElementById('root'));
+// var element = React.createElement('h1', null, 
+// React.createElement('em', null, 'Hello italic react'))
+
+// var children = [
+//  "Username",
+//  React.createElement("input", {type: 'text', name: 'username'}),
+//  "password",
+//   React.createElement("input", {type: 'text', name: 'password'})
+// ]
+
+// var div = React.createElement('form', null,
+
+// 	)
+
+// ReactDOM.render(element, document.getElementById('root'));
+
+// ReactDOM.render(children, document.getElementById('root'));
+
+
+// document.getElementById('id') === $('#id')
+
+var children = [
+  "Username",
+  React.createElement("input", {type: 'text', name: 'username'}),
+  "Password",
+  React.createElement("input", {type: 'password', name: 'password'}),
+  React.createElement("input", {type: 'submit'})
+];
+
+var div = React.createElement('form', null, "Username",
+  React.createElement("input", {type: 'text', name: 'username'}),
+  "Password",
+  React.createElement("input", {type: 'password', name: 'password'}),
+  React.createElement("input", {type: 'submit'}));
+
+ReactDOM.render(
+  div,
+  document.getElementById('out')
+);
