be92a04ef0498e0495c56e60b821a615f47883c7
Julian Mullins <mullinsjulian@gmail.com>
2016-07-05 16:57:45 -0400

    fjdkla

--- day1/custom-login/client/index.js
+++ day1/custom-login/client/index.js
@@ -3,11 +3,10 @@ var ReactDOM = require('react-dom');
 
 var MyForm = React.createClass({
   render: function() {
-    // Update this function and make this form customizable.
-    // Read values with {this.props.action}, {this.props.method} and
-    // {this.props.submitLabel}
-    // YOUR CODE HERE
-    return <form></form>;
+    return <form action={this.props.action} method={this.props.method}>
+    	Username: <input type="text" name="username"></input>
+    	Password: <input type="password" name="password"></input>
+    </form>;
   }
 });
 
