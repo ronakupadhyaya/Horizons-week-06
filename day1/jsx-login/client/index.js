9ed5565fa6624315eaf9abc1d3fb1861c5b38247
tconcannon <taycon@seas.upenn.edu>
2016-07-05 13:56:59 -0400

    inline

--- day1/jsx-login/client/index.js
+++ day1/jsx-login/client/index.js
@@ -7,5 +7,10 @@ var ReactDOM = require('react-dom');
 //   Password: <input type="password" name="password">
 //   <input type="submit">
 // </form>
-var element = <form></form>; // YOUR CODE INSIDE THE FORM
+var element = <form>
+Username: <input type="text" name="username"/>
+Password: <input type="password" name="password" />
+<input type="submit" />
+</form>; 
+// YOUR CODE INSIDE THE FORM
 ReactDOM.render(element, document.getElementById('root'));
