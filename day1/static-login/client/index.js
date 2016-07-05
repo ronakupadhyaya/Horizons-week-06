31e2faa543e0b7dccc0b971167f961997c6abfa1
Lucas <lmandelbaum1@babson.edu>
2016-07-05 13:58:48 -0400

    blah

--- day1/static-login/client/index.js
+++ day1/static-login/client/index.js
@@ -7,6 +7,15 @@ var ReactDOM = require('react-dom');
 //   Password: <input type="password" name="password">
 //   <input type="submit">
 // </form>
+
+//not done
+// var children = [
+// 	"Username",
+// 	React.createElement("input", {type:'text', name: 'username'}),
+// 		"Password",
+// 		React.createElement("input")
+// ]
+
 var element = React.createElement('form', /* YOUR CODE HERE */);
 
 ReactDOM.render(element, document.getElementById('root'));
