ed4876a6a73953f546eba3f16c3a7a6f7baf1d5a
spark4 <spark4@wellesley.edu>
2016-07-05 13:47:43 -0400

    kfajd;sakljf

--- day1/custom-login/client/index.js
+++ day1/custom-login/client/index.js
@@ -7,8 +7,7 @@ var MyForm = React.createClass({
     // Read values with {this.props.action}, {this.props.method} and
     // {this.props.submitLabel}
     // YOUR CODE HERE
-    return 
-    <form action={this.props.action} method = {this.props.method}>
+    return <form action={this.props.action} method = {this.props.method}>
     	Username: <input type="text" name = "username"/>
     	Password: <input type="password" name = "password"/>
     	<input type="submit" value = {this.props.submitLabel}/>
