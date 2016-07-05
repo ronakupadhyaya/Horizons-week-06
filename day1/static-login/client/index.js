f98ab9363fa34e4155e917a05280de997e44e564
Serena Woodward <smew820@gmail.com>
2016-07-05 13:54:39 -0400

    stuff

--- day1/static-login/client/index.js
+++ day1/static-login/client/index.js
@@ -7,11 +7,21 @@ var ReactDOM = require('react-dom');
 //   Password: <input type="password" name="password">
 //   <input type="submit">
 // </form>
-var element = React.createElement('form', ,
-  React.createElement('input', {type: 'text', name: 'username'}, 'username'),
-  React.createElement('input', {type: 'password', name: 'password'}, 'password'),
-  React.createElement('input', {type: 'submit'})
-  );
+var children = [
+  "Username",
+  React.createElement("input", {type: 'text', name: 'username'}),
+  "Password",
+  React.createElement("input", {type: 'password', name: 'password'}),
+  React.createElement("input", {type: 'submit'})
+];
 
-var div = React.createElement('form')
-ReactDOM.render(element, document.getElementById('root'));
+var div = React.createElement('form', null, "Username",
+  React.createElement("input", {type: 'text', name: 'username'}),
+  "Password",
+  React.createElement("input", {type: 'password', name: 'password'}),
+  React.createElement("input", {type: 'submit'}));
+
+ReactDOM.render(
+  div,
+  document.getElementById('out')
+);
