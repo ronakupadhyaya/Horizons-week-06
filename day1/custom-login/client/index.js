2834a0d45aeb1374a6caa8b6a29ab752bb03c4a9
Julian Mullins <mullinsjulian@gmail.com>
2016-07-05 20:22:04 -0400

    custom login

--- day1/custom-login/client/index.js
+++ day1/custom-login/client/index.js
@@ -6,9 +6,19 @@ var MyForm = React.createClass({
     return <form action={this.props.action} method={this.props.method}>
     	Username: <input type="text" name="username"></input>
     	Password: <input type="password" name="password"></input>
+    	<submitLabel = {this.props.submitLabel} />
     </form>;
   }
 });
 
 ReactDOM.render(<MyForm action="/test" method="post" submitLabel="Test submit"/>,
                 document.getElementById('root'));
+
+
+//     return <form action = {this.props.action} method = {this.props.method}>
+//     Username: <input type = "text" name = "username"/> 
+//     Password: <input type = "password" name = "password"/> 
+//     <submitLabel = {this.props.submitLabel}/>
+//     </form>;
+//   }
+// });
