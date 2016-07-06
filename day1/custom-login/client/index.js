999eae2fc1cd2bbe67d89cbedfe5b4140b7da229
AndyMartin93 <amarti40@villanova.edu>
2016-07-06 09:06:29 -0400

    practice

--- day1/custom-login/client/index.js
+++ day1/custom-login/client/index.js
@@ -7,7 +7,8 @@ var MyForm = React.createClass({
     // Read values with {this.props.action}, {this.props.method} and
     // {this.props.submitLabel}
     // YOUR CODE HERE
-    return <form></form>;
+
+    return <form action="{this.props.action}" method="{this.props.method}"></form>;
   }
 });
 
