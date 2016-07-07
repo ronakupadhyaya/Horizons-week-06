716f25db0909a8b3af5a77a6eba998c403dce900
Eagle Wu <eaglewu014@gmail.com>
2016-07-07 10:19:50 -0400

    updatez

--- day1/static-login/client/index.js
+++ day1/static-login/client/index.js
@@ -1,12 +1,13 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-// Update this call to .createElement and add these elements as children:
-// <form>
-//   Username: <input type="text" name="username">
-//   Password: <input type="password" name="password">
-//   <input type="submit">
-// </form>
-var element = React.createElement('form', /* YOUR CODE HERE */);
+var children = [
+	"Username",
+	React.createElement("input", {type: 'text', name: 'username'}),
+	"Password", 
+	React.createElement("input", {type: 'password', name: 'password'}),
+	React.createElement("input", {type: 'submit', value: 'submit'})
+] 
+var element = React.createElement('form', null, children);
 
 ReactDOM.render(element, document.getElementById('root'));
