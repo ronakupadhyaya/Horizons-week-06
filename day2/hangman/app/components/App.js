1450b7e634719b41c8f3f0263da7ef6627a60936
caroline <carokun@sas.upenn.edu>
2017-07-06 11:49:01 -0700

    first

--- day2/hangman/app/components/App.js
+++ day2/hangman/app/components/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-
+import { Link, Route } from 'react-router-dom';
 import GameContainer from '../containers/GameContainer';
+import About from './About';
+
 
 const App = () =>
     <div>
         <h1>Redux Hangman</h1>
-        <GameContainer />
+        <li><Link to="/">Game</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <Route path="/" exact component={GameContainer}/>
+        <Route path="/about" exact component={About}/>
     </div>;
 
 export default App;
