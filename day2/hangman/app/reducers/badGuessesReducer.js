80cc72b050a1d51a206b0bb78f4e2c1aa5285c79
Cyrus Behroozi <cyrus.behr@gmail.com>
2017-07-06 14:24:27 -0700

    completed step 4

--- day2/hangman/app/reducers/badGuessesReducer.js
+++ day2/hangman/app/reducers/badGuessesReducer.js
@@ -1,7 +1,7 @@
 /* Reducer for the badGuesses substate */
 /* This reducer's state will be a simple integer */
 
-import * as types from '../actions/types';
+// import * as types from '../actions/types';
 
 const badGuessesReducer = (state = 0, action) => {
     switch (action.type) {
