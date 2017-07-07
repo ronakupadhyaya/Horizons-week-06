050bad59850b7d0c6e3717339408dfc7617a04c4
David Huang <davidhuang972@gmail.com>
2017-07-06 21:57:41 -0700

    Add main functionality

--- day2/hangman/app/components/App.js
+++ day2/hangman/app/components/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Link, Route } from 'react-router-dom';
 
 import GameContainer from '../containers/GameContainer';
+import About from './About';
 
 const App = () =>
     <div>
         <h1>Redux Hangman</h1>
-        <GameContainer />
+        <Link to="/about">About</Link>
+        <br></br>
+        <Link to="/">Game</Link>
+        <Route exact={true} path="/" render={() => <GameContainer /> }/>
+        <Route exact={true} path="/about" component= {About} />
+
     </div>;
 
 export default App;
