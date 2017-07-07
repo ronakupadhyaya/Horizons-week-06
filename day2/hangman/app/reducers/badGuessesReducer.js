34196a3cc3717437fbbe601b8230a2826765fcc6
coreykeyser <keyser.57@osu.edu>
2017-07-07 11:36:31 -0700

    day 3 begin

--- day2/hangman/app/reducers/badGuessesReducer.js
+++ day2/hangman/app/reducers/badGuessesReducer.js
@@ -1,8 +1,17 @@
 /* Reducer for the badGuesses substate */
+
+const badGuessesReducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'BAD_GUESS':
+            return state + 1;
+        default:
+            return state;
+    }
+};
+
+export default badGuessesReducer;
+
+
 /* This reducer's state will be a simple integer */
 
 // import * as types from '../actions/types';
-
-// const badGuessesReducer =
-
-// export default badGuessesReducer;
--- day2/hangman/app/reducers/guessedLettersReducer.js
+++ day2/hangman/app/reducers/guessedLettersReducer.js
@@ -7,24 +7,11 @@
 // for each of the letter objects with letter: "O".
 // Your initial state can be any hardcoded word you want, just be sure all the
 // guessed fields start as false.
-const wordLettersReducer = (state = [
-            {letter: 'H', guessed: false},
-            {letter: 'O', guessed: false},
-            {letter: 'R', guessed: false},
-            {letter: 'I', guessed: false},
-            {letter: 'Z', guessed: false},
-            {letter: 'O', guessed: false},
-            {letter: 'N', guessed: false},
-            {letter: 'S', guessed: false}
-], action) => {
+const guessedLettersReducer = (state = [], action) => {
     switch (action.type) {
-        case 'GOOD_GUESS':
+        case 'BAD_GUESS':
             const newState = [...state];
-            newState.forEach((x) => {
-                if (x.letter === action.letter) {
-                    x.guessed = true;
-                }
-            });
+            newState.push(action.letter);
             return newState;
         default:
             return state;
@@ -33,4 +20,4 @@ const wordLettersReducer = (state = [
 // import * as types from '../actions/types';
 
 
-export default wordLettersReducer;
+export default guessedLettersReducer;
