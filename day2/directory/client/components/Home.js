5d1fd0f94ba4953f8c27515589899d949d67312f
segalb <none>
2017-07-07 09:04:27 -0700

    begining step 2

--- day2/directory/client/components/Home.js
+++ day2/directory/client/components/Home.js
@@ -1,16 +1,16 @@
 import React from 'react';
-//import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 class Home extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Welcome to the Horizons Directory</h1>
-        <h2>Click the link below to go to the directory.</h2>
-        {/* Link to /directory here */} 
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <h1>Welcome to the Horizons Directory</h1>
+                <h2>Click the link below to go to the directory.</h2>
+                <Link to="/directory">directory</Link>
+            </div>
+        )
+    }
 };
 
 export default Home;
