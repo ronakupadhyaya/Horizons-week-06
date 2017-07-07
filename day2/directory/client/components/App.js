7be6c16287d22f740168cd29d25ad964df230610
maxwell3739 <maxwell@uvic.ca>
2017-07-06 22:33:05 -0700

    push directory

--- day2/directory/client/components/App.js
+++ day2/directory/client/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch /*, Link */ } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link} from 'react-router-dom';
 
 import Home from './Home';
 import Directory from './Directory';
@@ -11,10 +11,12 @@ class App extends React.Component {
         <div>
           {/* Normally multiple routes can match at once.*/}
           {/* In a Switch, only the 1st matched route renders.*/}
+          <Route path="/:any" render={() => <Link to='/'>Back to Home</Link>}/>
           <Switch>
             {/* Your routes here */}
-
-
+          <Route exact={true} path="/" component={Home}/>
+          <Route path="/directory" component={Directory}/>
+          {/* <Route path='/directory/:firstname/:lastname' component={Directory} /> */}
             {/* A route with no path is matched unconditionally.*/}
             <Route render={() => <h1>404</h1>} />
           </Switch>
