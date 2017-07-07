00fc4208e8c0683a9282342ef17887b7edb5bdbe
allaunyc <alau11@binghamton.edu>
2017-07-07 13:02:56 -0700

    directory part 2 in progress

--- day2/directory/client/components/Directory.js
+++ day2/directory/client/components/Directory.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { /* Route, */ Link } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 const ppl = [
   { "fName": "Nihar", "lName": "Patil", "number": "(921)-664-2091", "email": "nihar@joinhorizons.com" },
@@ -25,7 +25,7 @@ class Directory extends React.Component {
     return (
       <div>
         <h1>Horizons Directory</h1>
-
+        <Route path="/directory" render={() => <LinkList links={ppl.map(pplToFullLink)}/>}/>
 
 
 
