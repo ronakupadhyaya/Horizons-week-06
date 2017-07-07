d6de2940c4d2b8f162267305a40b92c3b8c8a1e8
teresaliu20 <teresali@usc.edu>
2017-07-06 20:10:03 -0700

    Done

--- day2/directory/client/components/App.js
+++ day2/directory/client/components/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch /*, Link */ } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 
 import Home from './Home';
-import Directory from './Directory';
+import { Directory, Person} from './Directory';
 
 class App extends React.Component {
   render() {
@@ -11,10 +11,18 @@ class App extends React.Component {
         <div>
           {/* Normally multiple routes can match at once.*/}
           {/* In a Switch, only the 1st matched route renders.*/}
+          <Route path='/:page' render={() => <button><Link to='/'>Back Home</Link></button>}/>
+          <Route path='/directory' render={(p) => {
+            if (p.location.search === '') {
+              return <span></span>;
+            }
+            return <button><Link to='/directory'>Back to Directory</Link></button>
+          }}/>
           <Switch>
             {/* Your routes here */}
-
-
+            <Route path='/' exact component={Home}/>
+            <Route path='/directory' exact component={Directory}/>
+            <Route path='/directory/:fName/:lName' exact component={Person}/>
             {/* A route with no path is matched unconditionally.*/}
             <Route render={() => <h1>404</h1>} />
           </Switch>
