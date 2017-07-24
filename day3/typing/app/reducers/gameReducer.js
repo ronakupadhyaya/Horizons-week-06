e91f727664cd9cbf66344001f1daad72ea60c288
Miguelangel De Armas <dearmasm@wharton.upenn.edu>
2017-07-24 14:11:24 -0700

    Developing gameReducer

--- day3/typing/app/reducers/gameReducer.js
+++ day3/typing/app/reducers/gameReducer.js
@@ -1,4 +1,12 @@
 import words from '../dictionary'
 import _ from 'underscore'
 
-var 
+var shuffleWords = _.shuffle(words);
+var hundredWords = shuffleWords.slice(0, 100);
+
+const gameReducer = (state = hundredWords, action) {
+  switch(action.type) {
+    case " "
+      return ;
+  }
+}
