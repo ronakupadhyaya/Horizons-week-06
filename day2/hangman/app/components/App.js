5a71969593fe7738a8cdc8c2d4acfbd4a9e71bc6
jeff <tangj1122@gmail.com>
2017-10-10 12:04:27 -0700

    part1 links & static routes for components

--- day2/hangman/app/components/App.js
+++ day2/hangman/app/components/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-
+import {Link, Route} from 'react-router-dom';
 import GameContainer from '../containers/GameContainer';
+
+const footerLinks = ['About'];
 
 const App = () =>
     <div>
         <h1>Redux Hangman</h1>
-        <GameContainer />
+        <Route exact path="/" component={GameContainer}/>
+        <ul>
+          <li key="game"><Link to="/">Game</Link></li>
+          {footerLinks.map((link) => {
+              return (
+                <li key={link}><Link to={'/' + link}>{link}</Link></li>
+              );
+          })}
+        </ul>
     </div>;
 
 export default App;
