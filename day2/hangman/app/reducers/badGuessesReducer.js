c0cca686c1d4619a49d2384417f8a1e119cdf66c
jeff <tangj1122@gmail.com>
2017-10-10 13:36:39 -0700

    fin part 3: action badGuessesReducer

--- day2/hangman/app/reducers/badGuessesReducer.js
+++ day2/hangman/app/reducers/badGuessesReducer.js
@@ -3,6 +3,14 @@
 
 // import * as types from '../actions/types';
 
-// const badGuessesReducer =
+function badGuessesReducer(state = 0, action) {
+    switch (action.type) {
+        case 'BAD_GUESS':
+            return state + 1; // state++ is a no-no; do NOT modify previous state
+        default:
+            return state;
+    }
+}
 
-// export default badGuessesReducer;
+
+export default badGuessesReducer;
