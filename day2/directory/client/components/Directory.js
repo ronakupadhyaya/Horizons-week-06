b6e422adafceef9e140c90023b2b4db028cfa15e
Kobie <spaet062@umn.edu>
2017-10-11 09:46:15 -0700

    finish day 2

--- day2/directory/client/components/Directory.js
+++ day2/directory/client/components/Directory.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { /* Route, */ Link } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 
 const ppl = [
   { "fName": "Nihar", "lName": "Patil", "number": "(921)-664-2091", "email": "nihar@joinhorizons.com" },
@@ -10,14 +10,15 @@ const ppl = [
   { "fName": "Graham", "lName": "Master", "number": "(612)-284-3678", "email": "senseibaybay@aol.com" },
   { "fName": "Graham", "lName": "Cracker", "number": "(616)-824-0567", "email": "cinnamonbear@yahoo.com" },
   { "fName": "Darwish", "lName": "Gani", "number": "(774)-123-6477", "email": "darwish@joinhorizons.com" },
+  { "fName": "Bob", "lName": "Gani", "number": "(774)-123-9999", "email": "darwish@joinhorizons.com" },
   { "fName": "Darwish", "lName": "Gandhi", "number": "(124)-233-6755", "email": "saltypeace@india.gov" }
 
 ];
 
 const pplToFullLink = person => ({
-    to: `/directory/${person.fName}/${person.lName}`,
-    text: `${person.fName} ${person.lName}`,
-    key: person.number
+  to: `/directory/${person.fName}/${person.lName}`,
+  text: `${person.fName} ${person.lName}`,
+  key: person.number
 });
 
 class Directory extends React.Component {
@@ -25,51 +26,71 @@ class Directory extends React.Component {
     return (
       <div>
         <h1>Horizons Directory</h1>
+        <Switch>
+          <Route path='/directory' exact render={ () => <LinkList links={ppl.map(pplToFullLink)} />} />
+          <Route path='/directory/surname/:lName' render={ (props) => <LinkList links={ppl
+            .filter(p => p.lName === props.match.params.lName)
+            .map(pplToFullLink)}/> } />
+          <Route path='/directory/areacode/:number' render={ (props) => <LinkList links={ppl
+            .filter(p => p.number.slice(1, 4) === props.match.params.number)
+            .map(pplToFullLink)}/> } />
+          <Route path='/directory/:fName/:lName' component={Person} /* render={ ({match})=> <Person match={match}/>} */ />
+          <Route path='/directory/:fName' render={ (props) => <LinkList links={ppl
+            .filter(p => p.fName === props.match.params.fName)
+            .map(pplToFullLink)} />} />
+        </Switch>
+        <Route path='/directory/:anything' render={ () => <Link to='/directory'>Navigate back to all listings</Link> } />
 
+        </div>
+      );
+    }
+  };
 
+  class LinkList extends React.Component {
+    render() {
+      return (
+        this.props.links.length === 0 ? <h2>Nobody there!</h2> :
+          <ul>
+            {this.props.links.map(link => (
+              <li key={link.key}>
+                <Link to={link.to}>{link.text}</Link>
+              </li>
+            ))}
+          </ul>
 
+      );
+    }
+  };
 
-      </div>
-    );
+  class Person extends React.Component {
+    render() {
+      const person = ppl.find(p => (
+        p.fName === this.props.match.params.fName &&
+        p.lName === this.props.match.params.lName
+      ));
+
+      return person ? (
+        <div>
+          <h2>{`${person.fName} ${person.lName}`}</h2>
+          <h3>{person.number}</h3>
+          <h3>{person.email}</h3>
+
+          Not the {`${person.fName}`} you're looking for? {' '}
+          <div>
+            <Link to={'/directory/' + person.fName}>Find others with the same first name </Link>
+          </div>
+          <div>
+            <Link to={'/directory/surname/' + person.lName}>Find others with the same last name</Link>
+          </div>
+          <div>
+            <Link to={'/directory/areacode/' + person.number.slice(1, 4)}>Find others in the same area</Link>
+          </div>
+        </div>
+      ) : (
+        <h2>No {`${person.fName} ${person.lName}`} was found.</h2>
+      )
+    }
   }
-};
-
-class LinkList extends React.Component {
-  render() {
-    return (
-        <ul>
-          {this.props.links.map(link => (
-            <li key={link.key}>
-              <Link to={link.to}>{link.text}</Link>
-            </li>
-          ))}
-        </ul>
-    );
-  }
-};
-
-class Person extends React.Component {
-  render() {
-    // Array.prototype.find returns the first item satisfying the fn
-    const person = ppl.find(p => (
-      p.fName === this.props.match.params.fName &&
-      p.lName === this.props.match.params.lName
-    ));
-
-    return person ? (
-      <div>
-        <h2>{`${person.fName} ${person.lName}`}</h2>
-        <h3>{person.number}</h3>
-        <h3>{person.email}</h3>
-
-        Not the {`${person.fName}`} you're looking for? {' '}
-        Too bad!!!
-      </div>
-    ) : (
-      <h2>No {`${person.fName} ${person.lName}`} was found.</h2>
-    )
-  }
-}
 
 
-export default Directory;
+  export default Directory;
